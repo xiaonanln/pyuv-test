86340ce5359a368e29e7ae23e8bbe999ac65c30d
Ubuntu <ubuntu@ip-172-31-2-158.us-west-2.compute.internal>
2017-11-21 15:53:07 +0000

    test

--- test_echo_server_asyncore.py
+++ test_echo_server_asyncore.py
@@ -5,7 +5,6 @@ class EchoHandler(asyncore.dispatcher_with_send):
 
     def handle_read(self):
         data = self.recv(8192)
-        print len(data)
         if data:
             self.send(data)
 
